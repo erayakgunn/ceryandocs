d3150869721c579b43fac336d413132f80ec746f
erayakgun <erayakgun1@outlook.com>
2024-03-11 00:01:13 +0300

    fix analytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,8 +2,11 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { inject } from '@vercel/analytics';
+
 import './style.css'
 
+inject();
 export default {
   extends: DefaultTheme,
   Layout: () => {
